d602b9e7ab4e41adf5b9110ce8152d47420e4c82
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2019-11-26 15:42:10 -0500

    Updates

---
+++ precache-manifest.73a378b6a28f5aa05425943b22ac63f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be011ede5b8feb37af324f63c21ad8e7",
+    "url": "/stern-folio/index.html"
+  },
+  {
+    "revision": "19dee13c8a861083ea56",
+    "url": "/stern-folio/static/css/main.213bfceb.chunk.css"
+  },
+  {
+    "revision": "0f65df3b56a7c52578db",
+    "url": "/stern-folio/static/js/2.ea34dee6.chunk.js"
+  },
+  {
+    "revision": "19dee13c8a861083ea56",
+    "url": "/stern-folio/static/js/main.ee7d09d4.chunk.js"
+  },
+  {
+    "revision": "3ade99db9824b144d3cf",
+    "url": "/stern-folio/static/js/runtime-main.5798ce12.js"
+  }
+]);
