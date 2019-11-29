74a8ca04497443639ecde2906993ce3725c7820a
Ankit Curchorcar <ankit.curchorcar@gmail.com>
2019-11-29 09:50:52 -0500

    fixed react router issue with ghp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-// import { Route, Switch, BrowserRouter } from 'react-router-dom'
+import { Route, Switch, BrowserRouter } from 'react-router-dom'
 import Header from './components/Header'
 import About from './components/About'
 import Resume from './components/Resume'
@@ -8,8 +8,7 @@ import Skills from './components/Skills'
 // import Testimonials from './components/Testimonials'
 import ContactMe from './components/ContactMe'
 import Footer from './components/Footer'
-// import SplashPage from './login/SplashPage.jsx'
-// import NotFound from './new/Error404'
+import NotFound from './new/Error404'
 
 import personalData from './json/personalData'
 import resumeData from './json/resumeData'
@@ -21,16 +20,16 @@ class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				<Header jsonData={personalData} />
+				{/* <Header jsonData={personalData} />
 				<About jsonData={personalData} />
 				<Resume jsonData={resumeData} />
 				<Projects jsonData={projectData} />
 				<Skills jsonData={skillsData} />
-				{/* <Testimonials jsonData={contactData} /> */}
+				<Testimonials jsonData={contactData} />
 				<ContactMe jsonData={contactData} />
-				<Footer jsonData={personalData} />
+				<Footer jsonData={personalData} /> */}
 
-				{/* <BrowserRouter>
+				<BrowserRouter basename={process.env.PUBLIC_URL}>
 					<Switch>
 						<Route exact path='/'>
 							<Header jsonData={personalData} />
@@ -41,17 +40,17 @@ class App extends Component {
 							<ContactMe jsonData={contactData} />
 							<Footer jsonData={personalData} />
 						</Route>
-						<Route path='/resume'>
+						{/* <Route path='/resume'>
 							<Resume jsonData={resumeData} />
 						</Route>
 						<Route path='/testimonials'>
 							<Testimonials jsonData={contactData} />
-						</Route>
+						</Route> */}
 						<Route path='*'>
 							<NotFound jsonData={personalData} />
 						</Route>
 					</Switch>
-				</BrowserRouter> */}
+				</BrowserRouter>
 			</div>
 		)
 	}
